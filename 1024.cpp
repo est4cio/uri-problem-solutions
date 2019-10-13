52e048f6035a9d76f6fef0da2af20056d6ad6676
Gabriel Estácio <32314746+est4cio@users.noreply.github.com>
2019-10-13 09:28:52 -0300

    Update 1024.cpp

--- 1024.cpp
+++ 1024.cpp
@@ -1,38 +1,35 @@
 #include <stdio.h>
 #include <string.h>
-#include <iostream>
 
-void reverse(char word[],int len)
-{
+void cont(char frase[],int l){
     char temp;
-    for (int i=0;i<len/2;i++)
-    {
-            temp=word[i];
-            word[i]=word[len-i-1];
-            word[len-i-1]=temp;
+    
+    for (int i=0;i<l/2;i++){
+        temp=frase[i];
+        frase[i]=frase[l-i-1];
+        frase[l-i-1]=temp;
     }
 }
 
 int main(){
-    int n,i,j,len;
+    int n,i,j,l;
     char linha[1001];
     
     scanf("%d",&n);
     std::cin.getline(linha,0);
     for(i = 0; i < n; i++){
-            std::cin.getline(linha,1001);
-            
-            for(j = 0; linha[j] != '\0'; j++)
-                  if(linha[j] >= 65 && linha[j] <= 90 ||  linha[j] >= 97 && linha[j] <= 122)
-                              linha[j] += 3;
-            
-            len=strlen(linha);
-            reverse(linha,len);
-            
-            for(j = len/2; linha[j] != '\0'; j++)
-                              linha[j]--;
-            
-            printf("%s\n",linha);              
+        std::cin.getline(linha,1001);    
+        for(j = 0; linha[j] != '\0'; j++){
+            if(linha[j] >= 65 && linha[j] <= 90 ||  linha[j] >= 97 && linha[j] <= 122){
+                linha[j] += 3;
+            }
+            l=strl(linha);
+            cont(linha,len);
+        }
+        for(j = l/2; linha[j] != '\0'; j++){
+            linha[j]--;
+        }
+        printf("%s\n",linha);              
     } 
     return 0;
 }
